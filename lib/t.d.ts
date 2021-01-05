e2c8e7278161f2e8214a891839777930f1925b5a
alihsaas <alihsaas2@gmail.com>
2021-01-05 13:50:49 +0200

    added t Enum EnumItem and enum to t.d.ts

--- lib/t.d.ts
+++ lib/t.d.ts
@@ -86,6 +86,14 @@ interface t {
 	/** checks to see if `value` is a RBXScriptConnection */
 	RBXScriptConnection: t.check<RBXScriptConnection>;
 
+	// roblox enum types
+	/** checks to see if `value` is an Enum */
+	Enum: t.check<Enum>;
+	/** checks to see if `value` is an EnumItem */
+	EnumItem: t.check<EnumItem>;
+	/** checks if `value` is an EnumItem which belongs to `Enum`. */
+	enum: (Enum: Enum) => t.check<EnumItem>;
+
 	// type functions
 	/** checks to see if `value == literalValue` */
 	literal<T extends Array<Literal>>(this: void, ...args: T): t.check<ArrayType<T>>;
